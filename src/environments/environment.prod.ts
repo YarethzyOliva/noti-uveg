f2bd06cad26bd356b7f9cbe63a3cfbd9603aa65e
Yarethzy Oliva <yarethzy.oliva@gmail.com>
2025-03-03 13:33:19 -0600

    Se agrega configuración de Firebase en los entornos de producción y desarrollo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC1Y21dv5ZZlhWX_g45VWY5cFO7kydYdUs",
+    authDomain: "ionic-firebase-bcc7f.firebaseapp.com",
+    projectId: "ionic-firebase-bcc7f",
+    storageBucket: "ionic-firebase-bcc7f.firebasestorage.app",
+    messagingSenderId: "133815072295",
+    appId: "1:133815072295:web:53c02c16778e1de7b9c002",
+    measurementId: "G-KMTWHJ342D"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,5 +9,5 @@ export const environment = {
       messagingSenderId: "133815072295",
       appId: "1:133815072295:web:53c02c16778e1de7b9c002",
       measurementId: "G-KMTWHJ342D"
-  },
+  }
 };
