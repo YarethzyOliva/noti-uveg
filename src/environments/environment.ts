8c34defcbd01e488ac903e61870be3e7b48652f9
Yarethzy Oliva <yarethzy.oliva@gmail.com>
2025-03-03 00:48:32 -0600

    Se agrega configuración de Firebase en el entorno de desarrollo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,13 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+      apiKey: "AIzaSyC1Y21dv5ZZlhWX_g45VWY5cFO7kydYdUs",
+      authDomain: "ionic-firebase-bcc7f.firebaseapp.com",
+      projectId: "ionic-firebase-bcc7f",
+      storageBucket: "ionic-firebase-bcc7f.firebasestorage.app",
+      messagingSenderId: "133815072295",
+      appId: "1:133815072295:web:53c02c16778e1de7b9c002",
+      measurementId: "G-KMTWHJ342D"
+  },
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
