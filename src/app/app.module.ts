b95888d63b27bd8db416c027ebd09c23516077d8
Yarethzy Oliva <yarethzy.oliva@gmail.com>
2025-03-03 00:44:26 -0600

    Se instala e implementa Firebase en este proyecto. Además se conecta con el proyecto y app ya definidos previamente en Firebase, "ionic-firebase"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({ projectId: "ionic-firebase-bcc7f", appId: "1:133815072295:web:53c02c16778e1de7b9c002", storageBucket: "ionic-firebase-bcc7f.firebasestorage.app", apiKey: "AIzaSyC1Y21dv5ZZlhWX_g45VWY5cFO7kydYdUs", authDomain: "ionic-firebase-bcc7f.firebaseapp.com", messagingSenderId: "133815072295", measurementId: "G-KMTWHJ342D" })), provideFirestore(() => getFirestore())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
